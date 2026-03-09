b9642c16195d2c0c82f22e8ef7ca04dc264c3629
1024daniel <xxltju324@gmail.com>
2026-03-09 10:17:08 +0000

    skip lightning indexer

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -1244,3 +1244,14 @@ def trans_nd_to_nz(cache_tensor: torch.Tensor):
     cache_tensor = cache_tensor.reshape(nz_shape[:-4] + [m1, m0, n1, n0])
     cache_tensor = cache_tensor.permute(*array_trans)
     return cache_tensor
+
+# TODO: Temporarily use enable_skip_li to enable skipping lightning indexer for first 2048 token of ds32.
+@lru_cache(maxsize=1)
+def enable_lightning_indexer_skip() -> bool:
+    from vllm.config import get_current_vllm_config
+
+    vllm_config = get_current_vllm_config()
+    is_ds_v32 = hasattr(vllm_config.model_config, "hf_text_config") and hasattr(
+        vllm_config.model_config.hf_text_config, "index_topk"
+    )
+    return bool(is_ds_v32 and vllm_config.additional_config.get("enable_lightning_indexer_skip", False))
